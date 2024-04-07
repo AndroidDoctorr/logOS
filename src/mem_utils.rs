c5b4805c2f6ad79ffe52db4e7e93914b9b24fbd0
AndroidDoctorr <gbartthenerd@gmail.com>
2024-04-06 23:36:59 -0400

    added the writer for print functions

---
+++ src/mem_utils.rs
@@ -0,0 +1,21 @@
+#[no_mangle]
+pub extern "C" fn memset(s: *mut u8, c: i32, n: usize) -> *mut u8 {
+    let mut i = 0;
+    while i < n {
+        unsafe {
+            *s.add(i) = c as u8;
+        }
+        i += 1;
+    }
+    s
+}
+
+#[no_mangle]
+pub extern "C" fn memcpy(dest: *mut u8, src: *const u8, n: usize) -> *mut u8 {
+    for i in 0..n {
+        unsafe {
+            *dest.add(i) = *src.add(i);
+        }
+    }
+    dest
+}
